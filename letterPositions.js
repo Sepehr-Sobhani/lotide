a2724631c3dc22b344a6ecd197222a547a48da57
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-11-25 21:52:41 +0000

    adding new function to keep all positions of a letter happening in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,31 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+  let output = true;
+  for (let i = 0; i < arrayOne.length; i++) {
+    arrayOne[i] !== arrayTwo[i] ? (output = false) : "";
+  }
+  return output;
+};
+
+const assertArraysEqual = (arrayOne, arrayTwo) => {
+  if (eqArrays(arrayOne, arrayTwo))
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  else console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  let indexCounter = 0;
+  for (const char of sentence) {
+    if (char !== " ") {
+      if (results[char]) {
+        results[char].push(indexCounter);
+      } else {
+        results[char] = [indexCounter];
+      }
+    }
+    indexCounter++;
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
