a7b47abc92e355e127e00c11d57617d1c62d84f6
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-11-25 22:06:24 +0000

    creating a new function which takes in an object and a value and return the fist key which contains the given value

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, value) => {
+  let outputKey;
+  for (const key in obj) {
+    if (obj[key] === value) outputKey = key;
+  }
+  return outputKey;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
