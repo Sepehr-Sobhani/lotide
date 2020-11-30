dc54c82533217d8df93d723fdb38c23998286146
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-11-30 20:32:07 +0000

    adding tailTest file for testing tail file to the test folder

--- tail.js
+++ tail.js
@@ -1,16 +1,5 @@
-const assertEqual = function (actual, expected) {
-  actual === expected
-    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const tail = (array) => {
   return array.length <= 1 ? [] : array.slice(1);
 };
 
-assertEqual(tail([1]), []);
-assertEqual(tail([]), []);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
