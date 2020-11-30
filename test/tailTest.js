5f62d4e23b8853634de2693ca212d24e36ab97ae
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-11-30 22:22:52 +0000

    adding mocha and chai test cases for tail and middle functions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,15 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
-const eqArrays = require("../eqArrays");
+const assert = require("chai").assert;
 
-assertEqual(tail([1]), []);
-assertEqual(tail([]), []);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
