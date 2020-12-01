6632114076e5b7de7f95253685798e6899dd54a3
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-01 01:02:03 +0000

    adding mocha and chai testing to flatten function

--- flatten.js
+++ flatten.js
@@ -1,17 +1,3 @@
-const eqArrays = (arrayOne, arrayTwo) => {
-  let output = true;
-  for (let i = 0; i < arrayOne.length; i++) {
-    arrayOne[i] !== arrayTwo[i] ? (output = false) : "";
-  }
-  return output;
-};
-
-const assertArraysEqual = (arrayOne, arrayTwo) => {
-  if (eqArrays(arrayOne, arrayTwo))
-    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
-  else console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
-};
-
 const flatten = (array) => {
   let flattenedArray = [];
   for (let value of array) {
@@ -21,8 +7,5 @@ const flatten = (array) => {
   }
   return flattenedArray;
 };
-
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
 
 module.exports = flatten;
